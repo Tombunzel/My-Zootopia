5ad397fcdacfa45102a5f4b9b154cbc66670f352
Tombunzel <bunzel.tom@gmail.com>
2024-08-26 13:57:05 +0200

    fetch animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -31,16 +31,38 @@ def get_animal_type(animal):
 
 
 def get_animal_key_data(animals_data):
+    output = ''
     for animal in animals_data:
         name = get_animal_name(animal)
         diet = get_animal_diet(animal)
         location = get_animal_location(animal)
         animal_type = get_animal_type(animal)
-        print(f"Name: {name}\nDiet: {diet}\nLocation: {location}\nType: {animal_type}\n")
+        output += f"Name: {name}\nDiet: {diet}\nLocation: {location}\nType: {animal_type}\n\n"
+    return output
+
+
+def load_html_data(file_path):
+    with open(file_path, 'r') as handle:
+        return handle.read()
+
+
+def create_final_output(template_data, animals_data):
+    final_output = template_data.replace("__REPLACE_ANIMALS_INFO__", animals_data)
+    return final_output
+
+
+def write_data_new_file(output, file_path):
+    with open(file_path, "w") as handle:
+        handle.write(output)
 
 
 def main():
-    get_animal_key_data(animals_data = load_data("animals_data.json"))
+    animals_data = get_animal_key_data(load_data("animals_data.json"))
+    template_data = load_html_data("animals_template.html")
+    final_output = create_final_output(template_data, animals_data)
+    write_data_new_file(final_output, "animals.html")
+
+    # print(get_animal_key_data(animals_data))
 
 
 if __name__ == "__main__":
