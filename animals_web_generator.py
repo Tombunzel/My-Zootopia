fc2fd54b984656ba5e3bedb21f26f051f8588f4b
Tombunzel <bunzel.tom@gmail.com>
2024-08-27 09:55:55 +0200

    Added Python features to create website according to chosen animal skin type

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -42,14 +42,16 @@ def serialize_animal(animal):
     location = get_animal_location(animal)
     animal_type = get_animal_type(animal)
     sci_name = get_animal_sci_name(animal)
-    output += '<li class="cards__item"><br>'
+    output += '<li class="cards__item">\n'
     output += f'<div class="card__title">{name}</div>\n'
     output += '<p class="card__text">\n'
-    output += f'<strong>Scientific Name:</strong> {sci_name}<br>\n'
-    output += f'<strong>Location:</strong> {location}<br>\n'
+    output += '<ul>\n'
+    output += f'<li><strong>Scientific Name:</strong> {sci_name}</li>\n'
+    output += f'<li><strong>Location:</strong> {location}</li>\n'
     if animal_type:
-        output += f'<strong>Type:</strong> {animal_type}<br>\n'
-    output += f'<strong>Diet:</strong> {diet}<br>\n'
+        output += f'<li><strong>Type:</strong> {animal_type}</li>\n'
+    output += f'<li><strong>Diet:</strong> {diet}</li>\n'
+    output += '</ul>\n'
     output += '</p>\n'
     output += '</li>\n'
     return output
@@ -59,6 +61,28 @@ def get_animal_key_data(animals_data):
     output = ''
     for animal in animals_data:
         output += serialize_animal(animal)
+    return output
+
+
+def get_skin_types_list(animals_data):
+    skin_types_list = []
+    for animal in animals_data:
+        skin_type = animal['characteristics']['skin_type']
+        skin_types_list.append(skin_type)
+    return skin_types_list
+
+
+def get_chosen_animal_data(animals_data):
+    output = ''
+    skin_types_list = get_skin_types_list(animals_data)
+    for skin_type in skin_types_list:
+        print(skin_type)
+    chosen_skin_type = input("Please enter exactly the skin type of your choice: ")
+    for animal in animals_data:
+        skin_type = animal['characteristics']['skin_type']
+        if skin_type == chosen_skin_type:
+            output += serialize_animal(animal)
+
     return output
 
 
@@ -78,12 +102,11 @@ def write_data_new_file(output, file_path):
 
 
 def main():
-    animals_data = get_animal_key_data(load_data("animals_data.json"))
+    animals_data = get_chosen_animal_data(load_data("animals_data.json"))
+    # animals_data = get_animal_key_data(load_data("animals_data.json"))
     template_data = load_html_data("animals_template.html")
     final_output = create_final_output(template_data, animals_data)
     write_data_new_file(final_output, "animals.html")
-
-    # print(get_animal_key_data(animals_data))
 
 
 if __name__ == "__main__":
